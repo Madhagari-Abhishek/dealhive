d2f68078fb818f56373cbe2baf3c7983f54b54c7
Madhagari Abhishek <150315979+Madhagari-Abhishek@users.noreply.github.com>
2025-02-23 19:55:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,46 @@
+// Sample product data
+const products = [
+    {
+        name: "Wireless Earbuds - Boat Immortal 111",
+        description: "This is a sample product description.",
+        image: "images/111.png",
+        link: "https://www.amazon.com/dp/B08N5WRWNW"
+    },
+    {
+        name: "Wireless keyboard - Zebronics",
+        description: "This is another sample product description.",
+        image: "images/zeb.png",
+        link: "https://www.flipkart.com/some-product"
+    },
+    {
+        name: "Noice Voetex Plus Smart Watch",
+        description: "Yet another sample product description.",
+        image: "images/noi.png",
+        link: "https://www.amazon.com/dp/B08N5WRWNW"
+    }
+    
+];
+
+// Function to create product cards
+function createProductCard(product) {
+    return `
+        <div class="col-md-4">
+            <div class="product-card">
+                <a href="${product.link}" target="_blank">
+                    <img src="${product.image}" alt="${product.name}">
+                    <h3>${product.name}</h3>
+                    <p>${product.description}</p>
+                </a>
+            </div>
+        </div>
+    `;
+}
+
+// Function to render product cards
+function renderProducts() {
+    const productGrid = document.getElementById('product-grid');
+    productGrid.innerHTML = products.map(createProductCard).join('');
+}
+
+// Render products on page load
+document.addEventListener('DOMContentLoaded', renderProducts);
